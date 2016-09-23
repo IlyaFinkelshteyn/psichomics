46b84ab53f84dec607bc2aa57dcb1a1eeb309235
Nuno Agostinho <nunodanielagostinho@gmail.com>
2016-09-24 00:20:18 +0100

    Fix AppVeyor error

--- R/data_firebrowse.R
+++ R/data_firebrowse.R
@@ -129,6 +129,7 @@ queryFirehoseData <- function(format = "json", date = NULL, cohort = NULL,
 #'
 #' @return List with parsed JSON response
 #'
+#' @import curl
 #' @importFrom httr GET content
 #' @importFrom jsonlite fromJSON
 #'
